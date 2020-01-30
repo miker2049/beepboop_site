0ec4bbda8bbbb3dff5ed5489e4c8899574ff64c4
mik rus <russomichaelb@gmail.com>
2020-01-30 15:56:42 -0500

    lots of css fixes, header dynamics

--- src/site/css/styles.11ty.js
+++ src/site/css/styles.11ty.js
@@ -24,6 +24,7 @@ module.exports = class {
             require('lost'),
             require('postcss-mixins'),
             require('postcss-color-mix'),
+            require('postcss-color-function'),
             require('postcss-mesh'),
             require('cssnano')
         ])
